89beaa67d4e4958534af99a4041e17cc3e90cde3
Emanuele Falzone <emafalzo@gmail.com>
2020-11-13 15:15:09 +0100

    Remove csv from dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='SPARQL2Spark',
       description='SPARQL Result to Spark',
       author='Emanuele Falzone',
       author_email='emanuele.falzone@polimi.it',
-      install_requires=['rdflib','rdflib-jsonld','sparqlwrapper','graphframes','pyspark','csv'],
+      install_requires=['rdflib','rdflib-jsonld','sparqlwrapper','graphframes','pyspark'],
       packages=['SPARQL2Spark'],
       zip_safe=False)
