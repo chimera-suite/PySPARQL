eae38e27d1e8ba00e032e093a239a5e16b547bca
Emanuele Falzone <emafalzo@gmail.com>
2020-11-13 14:50:43 +0100

    Update version and dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='SPARQL2Spark',
-      version='0.0.1',
-      description='SPARQL Result to Spark DataFrame',
+      version='0.0.2',
+      description='SPARQL Result to Spark',
       author='Emanuele Falzone',
       author_email='emanuele.falzone@polimi.it',
-      install_requires=['rdflib','rdflib-jsonld','sparqlwrapper'],
+      install_requires=['rdflib','rdflib-jsonld','sparqlwrapper','graphframes','pyspark','csv'],
       packages=['SPARQL2Spark'],
       zip_safe=False)
